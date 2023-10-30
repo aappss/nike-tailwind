0e25831245ab3f4d0ac7b0bb0b9e25509ba8de63
Abhishek Pratap Singh <abhi100sec@gmail.com>
2023-10-30 15:52:55 +0530

    UI changes

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Hero
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProduct from './PopularProduct';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export {
+    Hero,
+    PopularProduct,
+    Footer,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
